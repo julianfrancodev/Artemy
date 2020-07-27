a073961e39485b874ee18d75bed23fcb22081128
JULIAN <juliandanielfranco@gmail.com>
2020-07-26 22:10:55 -0500

    Add Transparent StatusBar

--- App.js
+++ App.js
@@ -3,14 +3,18 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OnBoarding from './src/Authentication/OnBoarding';
+import { StatusBar } from 'react-native';
 
 const AuthStack = createStackNavigator();
 
 const AuthenticationNavigator = () => {
   return (
-    <AuthStack.Navigator headerMode='none'>
-      <AuthStack.Screen name={"OnBoarding"} component={OnBoarding} />
-    </AuthStack.Navigator>
+    <>
+      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+      <AuthStack.Navigator headerMode='none'>
+        <AuthStack.Screen name={"OnBoarding"} component={OnBoarding} />
+      </AuthStack.Navigator>
+    </>
   )
 
 }
@@ -18,7 +22,7 @@ const AuthenticationNavigator = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthenticationNavigator/>
+      <AuthenticationNavigator />
     </NavigationContainer>
   );
 }
